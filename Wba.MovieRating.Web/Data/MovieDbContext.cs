a655164f0b22f87142858a83a4324ff35e5c4c3d
Mileto Di Marco <mileto.di.marco@howest.be>
2025-10-23 15:01:11 +0200

    Add project files.

---
+++ Wba.MovieRating.Web/Data/MovieDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wba.MovieRating.Core.Entities;
+
+namespace Wba.MovieRating.Web.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        //Dbsets = database tables
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<User> Users { get; set; }
+        public MovieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
