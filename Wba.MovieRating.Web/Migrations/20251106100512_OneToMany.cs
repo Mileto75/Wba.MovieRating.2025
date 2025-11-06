118e3497d9a28593d629090d0768b8612e8bff86
Mileto Di Marco <mileto.di.marco@howest.be>
2025-11-06 11:09:24 +0100

    Add migration

---
+++ Wba.MovieRating.Web/Migrations/20251106100512_OneToMany.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wba.MovieRating.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class OneToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_CompanyId",
+                table: "Movies",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Companies_CompanyId",
+                table: "Movies",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Companies_CompanyId",
+                table: "Movies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_CompanyId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "Movies");
+        }
+    }
+}
